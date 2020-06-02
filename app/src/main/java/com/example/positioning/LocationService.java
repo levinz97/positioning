05b4c2114d9f29e8e666e3b40232c1743e8f6f06
levinz <58430798+levinz97@users.noreply.github.com>
2020-06-02 09:52:27 +0200

    first commit

---
+++ app/src/main/java/com/example/positioning/LocationService.java
@@ -0,0 +1,232 @@
+package com.example.positioning;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.Service;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.HandlerThread;
+import android.os.IBinder;
+import android.os.Process;
+import android.provider.Settings;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import org.alternativevision.gpx.GPXParser;
+import org.alternativevision.gpx.beans.GPX;
+import org.alternativevision.gpx.beans.Waypoint;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.logging.Logger;
+
+import org.alternativevision.gpx.beans.Track;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+
+public class LocationService extends Service {
+    public static boolean isRunning = false;
+    private LocationListener locationListener;
+    private LocationManager locationManager;
+    private FileOutputStream out;
+    private Location lastLocation;
+    private float travelDistance = 0;
+    private long travelTime = 0;
+    private Date date1, date2;
+    private Waypoint waypoint;
+    private GPX gpx;
+    private GPXParser gpxParser;
+    public static final String
+            ACTION_LOCATION_BROADCAST = LocationService.class.getName() + "LocationBroadcast",
+            EXTRA_LATITUDE = "extra_latitude",
+            EXTRA_LONGITUDE = "extra_longitude",
+            EXTRA_DISTANCE = "extra_distance",
+            EXTRA_AVG_SPEED = "extra_avg_speed";
+
+    private static final int
+            MIN_TIME = 2000,
+            MIN_DISTANCE = 5;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        isRunning = true;
+        Log.e("Start", "onCreate");
+        Log.e("Start", String.valueOf(isRunning));
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
+        super.onCreate();
+    }
+
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Toast.makeText(this, "service starting", Toast.LENGTH_SHORT).show();
+        Log.e("Start", "onStartCommand");
+        Log.e("Start", "onStartCommand " + isRunning);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if (locationManager != null && !locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            enableLocationSettings();
+//              stopSelfResult(1);
+//              stopSelf();
+//              stopForeground(true);
+//              stopService(new Intent(this,LocationService.class));
+
+//              Log.e("stop", String.valueOf(stopSelfResult(1)));
+        }
+
+        lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        date1 = new Date(lastLocation.getTime());
+        travelTime = 0;
+        travelDistance = 0;
+        sendBroadcastMessage(lastLocation, 0, 0);
+        final ArrayList<Waypoint> waypointArrayList = new ArrayList<>();
+        final HashSet<Track> tracks = new HashSet<>();
+
+        waypoint = new Waypoint();
+        gpx = new GPX();
+        gpxParser = new GPXParser();
+        waypoint.setLatitude(lastLocation.getLatitude());
+        waypoint.setLongitude(lastLocation.getLongitude());
+        waypoint.setTime(date1);
+
+        final Track track = new Track();
+        track.setTrackPoints(waypointArrayList);
+        tracks.add(track);
+        waypointArrayList.add(waypoint);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                date2 = new Date(location.getTime());
+                travelTime += Math.abs(date2.getTime() - date1.getTime());
+                travelDistance += distance(lastLocation, location);
+                sendBroadcastMessage(location, travelDistance, avgSpeed(travelDistance, travelTime));
+
+                waypoint = new Waypoint();
+                waypoint.setLatitude(location.getLatitude());
+                waypoint.setLongitude(location.getLongitude());
+                waypoint.setTime(date2);
+                waypointArrayList.add(waypoint);
+                Log.i("waypoint", String.valueOf(waypoint));
+                track.setTrackPoints(waypointArrayList);//
+                tracks.clear();
+                tracks.add(track);
+                gpx.setTracks(tracks);//
+                try {
+                    out = new FileOutputStream("/sdcard/Download/map.gpx");
+                    gpxParser.writeGPX(gpx, out);
+
+                } catch (ParserConfigurationException e) {
+                    e.printStackTrace();
+                } catch (TransformerException | IOException e) {
+                    e.printStackTrace();
+                }
+                date1 = date2;
+                lastLocation = location;
+                Log.e("Location", String.valueOf(location.getLatitude()));
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                enableLocationSettings();
+            }
+        };
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME, MIN_DISTANCE, locationListener);
+        return START_REDELIVER_INTENT;
+    }
+
+
+    @Override
+    public void onDestroy() {
+        Toast.makeText(this, "cancelled", Toast.LENGTH_SHORT).show();
+        Log.e("Cancel", "onDestroy");
+        stopSelf();
+        isRunning = false;
+        locationManager.removeUpdates(locationListener);
+        try {
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        super.onDestroy();
+    }
+
+    private void sendBroadcastMessage(Location location, float distance, float speed) {
+        if (location != null) {
+            Intent intent = new Intent(ACTION_LOCATION_BROADCAST);
+            intent.putExtra(EXTRA_LATITUDE, location.getLatitude());
+            intent.putExtra(EXTRA_LONGITUDE, location.getLongitude());
+            intent.putExtra(EXTRA_DISTANCE, distance);
+            intent.putExtra(EXTRA_AVG_SPEED, speed);
+            this.sendBroadcast(intent);
+        }
+    }
+
+
+    private static float distance(Location location1, Location location2) {
+        double earthRadius = 6371000; //meters
+        double dLat = Math.toRadians(location2.getLatitude() - location1.getLatitude());
+        double dLng = Math.toRadians(location2.getLongitude() - location1.getLongitude());
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                Math.cos(Math.toRadians(location1.getLatitude())) * Math.cos(Math.toRadians(location2.getLatitude())) *
+                        Math.sin(dLng / 2) * Math.sin(dLng / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        float dist = (float) (earthRadius * c);
+        return dist / 1000;
+    }
+
+    private static float avgSpeed(float distance, long time) {
+        return (float) 3600000 * distance / time;
+    }
+
+    private void enableLocationSettings() {
+        new AlertDialog.Builder(LocationService.this)
+                .setTitle("enable GPS")
+                .setMessage("GPS is disabled")
+                .setPositiveButton("GPS settings", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent settingIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivity(settingIntent);
+                    }
+                })
+                .setNegativeButton("no", null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
+
+
+}
+
---
+++ app/src/main/java/com/example/positioning/MainActivity.java
@@ -0,0 +1,146 @@
+package com.example.positioning;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.ActivityManager;
+import android.app.AlertDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.alternativevision.gpx.GPXParser;
+import org.alternativevision.gpx.beans.GPX;
+
+import java.io.FileOutputStream;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button stBtn, stopBtn, uptBtn, exitBtn;
+    TextView laTxt, loTxt, disTxt, spTxt;
+    Intent intent;
+    BroadcastListener broadcastListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        laTxt = findViewById(R.id.laTxt);
+        loTxt = findViewById(R.id.loTxt);
+        disTxt = findViewById(R.id.disTxt);
+        spTxt = findViewById(R.id.spTxt);
+        stBtn = findViewById(R.id.stBtn);
+        stopBtn = findViewById(R.id.stopBtn);
+        uptBtn = findViewById(R.id.uptBtn);
+        exitBtn = findViewById(R.id.exitBtn);
+
+        if (shouldAskPermissions()) {
+            askPermissions();
+        }
+        broadcastListener = new BroadcastListener();
+        this.registerReceiver(
+               broadcastListener, new IntentFilter(LocationService.ACTION_LOCATION_BROADCAST)
+        );
+
+        uptBtn.setOnClickListener(this);
+        stBtn.setOnClickListener(this);
+        stopBtn.setOnClickListener(this);
+        exitBtn.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        intent = new Intent(this,LocationService.class);
+        switch (v.getId()){
+            case R.id.stBtn:
+                startService(intent);
+                break;
+            case R.id.stopBtn:
+                Log.e("Cancel", String.valueOf(isMyServiceRunning(LocationService.class)));
+                laTxt.setText("Latitude: " );
+                loTxt.setText("Longitude: " );
+                disTxt.setText("Distance: " );
+                spTxt.setText("Average Speed: " );
+                if (isMyServiceRunning(LocationService.class)){
+                    stopService(intent);
+                }
+                break;
+            case R.id.exitBtn:
+                unregisterReceiver(broadcastListener);
+                finish();
+                break;
+            case R.id.uptBtn:
+                laTxt.setText("Latitude: " + BroadcastListener.LA);
+                loTxt.setText("Longitude: " + BroadcastListener.LO);
+                disTxt.setText("Distance: " + BroadcastListener.DI + "km");
+                spTxt.setText("Average Speed: " + BroadcastListener.SP + "km/h");
+                break;
+            default:
+                break;
+        }
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+
+
+    @SuppressLint("NewApi")
+    protected void askPermissions() {
+        String[] permissions = {
+                "android.permission.ACCESS_FINE_LOCATION",
+                "android.permission.WRITE_EXTERNAL_STORAGE",
+                "android.permission.ACCESS_FINE_LOCATION",
+                "android.permission.ACCESS_COARSE_LOCATION"
+        };
+        int requestCode = 200;
+        requestPermissions(permissions, requestCode);
+    }
+
+
+    protected boolean shouldAskPermissions() {
+        return (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED );
+
+    }
+
+    private boolean isMyServiceRunning(Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
